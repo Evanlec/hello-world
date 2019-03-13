dc19a2b88bdeaafaedc72336988a0a0dad01ebed
Evan LeCompte <evanlec@gmail.com>
2019-03-13 04:05:55 -0400

    add appspec.yml and start.sh

---
+++ start.sh
@@ -0,0 +1,15 @@
+kill -TERM $(pgrep gunicorn)
+
+if [[ -d "$PWD/venv" ]]; then
+    rm -rf "$PWD/venv"
+fi
+
+python3 -m venv venv
+venv/bin/pip install -r requirements.txt
+
+venv/bin/gunicorn \
+    --daemon \
+    --workers 4 \
+    --error-logfile ./error-log.log
+    --reload \
+    hello-world:app
